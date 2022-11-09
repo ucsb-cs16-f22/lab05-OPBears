ad0d65380eb194248a7d4f484d3d387db5dcde32
Teddy Hwang <t_hwang@ucsb.edu>
2022-11-09 10:18:44 -0800

    distanceBetween works

--- shapeFuncs.cpp
+++ shapeFuncs.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <sstream>  // for ostringstream
 #include <iomanip> // for setprecision
-
+#include <cmath>
 using namespace std;
 
 
@@ -13,7 +13,9 @@ using namespace std;
 // Compute distance between two points
 
 double distanceBetween(Point p, Point q) {
-  return 99999.9; // @@@ STUB
+  double val = sqrt(pow((p.x - q.x),2)+pow((p.y - q.y),2));
+  
+  return val; // @@@ STUB
   // HINTS: The distance formula is something you hopefully remember
   //   from H.S. algebra, but if not, Wikipedia is your friend.
   //   The sqrt function is available when you use #include <cmath>, 
